efecb044837a30436280e440b182bee0176915bc
tejudharanesh <tejudharanesh1234@gmail.com>
2024-10-23 13:22:42 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+// WebRTC signaling
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
+
+  socket.on("offer", (data) => {
+    socket.broadcast.emit("offer", data);
+  });
+
+  socket.on("answer", (data) => {
+    socket.broadcast.emit("answer", data);
+  });
+
+  socket.on("ice-candidate", (data) => {
+    socket.broadcast.emit("ice-candidate", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
